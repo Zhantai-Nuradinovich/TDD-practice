4fb8e83d8c01e5d3eedcd00591fc8332a369df58
Zhantai <jantai.nuradinovich@gmail.com>
2022-07-19 15:35:39 +0600

    Add new case to StringUtility.Sum

--- TDD.Tests/TDDTests.cs
+++ TDD.Tests/TDDTests.cs
@@ -17,7 +17,8 @@ namespace Task1.Tests
         [TestCase("3", "0", ExpectedResult = 3)]
         [TestCase("3.5", "0", ExpectedResult = 0)]
         [TestCase("-3", "0", ExpectedResult = -3)]
-        //[TestCase("-3999", "-", ExpectedResult = -3999)]
+        [TestCase("-3999", "-", ExpectedResult = -3999)]
+        [TestCase("-##$#", "dss654", ExpectedResult = 0)]
         public int Sum_TwoCorrectNumbers_ReturnsSum(string num1, string num2)
         {
             return StringUtility.Sum(num1, num2);
