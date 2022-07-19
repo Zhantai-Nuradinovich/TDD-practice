2e611b2ca994ab249a9485f3653ae48dd4f75382
Zhantai <jantai.nuradinovich@gmail.com>
2022-07-19 15:24:26 +0600

    Add StringUtility.Sum tests

---
+++ TDD.Tests/TDDTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+using System.Linq;
+using TDD;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class TDDTests
+    {
+        [Test]
+        public void Sum_EmptyStrings_ThrowsArgumentException()
+        {
+            Assert.That(() => StringUtility.Sum("", ""), Throws.ArgumentException);
+        }
+
+        [TestCase("1", "1", ExpectedResult = 2)]
+        [TestCase("3", "0", ExpectedResult = 3)]
+        [TestCase("3.5", "0", ExpectedResult = 0)]
+        [TestCase("3", "0", ExpectedResult = 3)]
+        public int Sum_TwoCorrectNumbers_ReturnsSum(string num1, string num2)
+        {
+            return StringUtility.Sum(num1, num2);
+        }
+
+        [Test]
+        public void Sum_NullSource_ThrowsArgumentNullException()
+        {
+            Assert.That(() => StringUtility.Sum(null, "42"), Throws.ArgumentNullException);
+        }
+    }
+}
---
+++ TDD/StringUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD
+{
+    public static class StringUtility
+    {
+        public static int Sum(string num1, string num2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
